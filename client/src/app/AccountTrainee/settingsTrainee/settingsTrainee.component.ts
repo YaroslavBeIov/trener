ffd9d6e1b39c520add5a46897f65644f94addb55
Belov <bombatiktak@mail.ru>
2024-02-13 22:37:52 +0300

    +

--- client/src/app/AccountTrainee/settingsTrainee/settingsTrainee.component.ts
+++ client/src/app/AccountTrainee/settingsTrainee/settingsTrainee.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+interface User {
+  name: string;
+  age: string;
+  height: string;
+  weight: string;
+}
 
 @Component({
-  selector: 'app-settingstrainee',
+  selector: 'app-settingsTrainee',
   standalone: true,
-  imports: [],
   templateUrl: './settingsTrainee.component.html',
-  styleUrl: './settingsTrainee.component.css'
+  styleUrls: ['./settingsTrainee.component.css'],
+  imports: [FormsModule, CommonModule,HttpClientModule]
 })
-export class SettingstraineeComponent {
 
+export class SettingstraineeComponent {
+  user: User = {
+    name: "", //Добавить инфу о фотографии пользователя
+    age: "",
+    height: "",
+    weight: ""
+  };
+
+  constructor(private http: HttpClient) {}
+
+  saveSettings() {
+    this.http.post('http://example.com/save-user-settings', this.user)
+      .subscribe(
+        (response) => {
+          console.log('Настройки сохранены успешно', response);
+        },
+        (error) => {
+          console.error('Ошибка при сохранении настроек', error);
+        }
+      );
+  }
 }
+
